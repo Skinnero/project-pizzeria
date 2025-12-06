31936e46df59e82e31757627bdc18b86508ea8a2
Kacper Tomczyk <46459137+Skinnero@users.noreply.github.com>
2025-12-06 18:55:02 +0100

    Adding a 2nd page for booking

---
+++ src/js/components/Booking.js
@@ -0,0 +1,41 @@
+import {select, templates} from '../settings.js';
+import AmountWidget from './AmountWidget.js';
+
+class Booking {
+  constructor(element) {
+    const thisBooking = this;
+
+    thisBooking.render(element);
+    thisBooking.initWidgets();
+  }
+
+  render(element) {
+    const thisBooking = this;
+    const generatedHTML = templates.bookingWidget();
+
+    thisBooking.dom = {};
+
+    thisBooking.dom.wrapper = element;
+    thisBooking.dom.wrapper.innerHTML = generatedHTML;
+
+    thisBooking.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
+  }
+
+  initWidgets() {
+    const thisBooking = this;
+
+    console.log(thisBooking.dom);
+    thisBooking.peopleAmountWidget = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.dom.peopleAmount.addEventListener('updated', () => {
+
+    });
+
+    thisBooking.hoursAmountWidget = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.dom.hoursAmount.addEventListener('updated', () => {
+
+    });
+  }
+}
+
+export default Booking;
