6fbe4217ad101695410c9e75017cc3e18d865094
Kacper Tomczyk <46459137+Skinnero@users.noreply.github.com>
2025-12-09 12:31:19 +0100

    Adding DatePicker and HoursPicker with implementation to website, changing html default value to 1 form products so it will show initial number after changes, adding plugins for date and hours picker

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,7 @@
 import {select, templates} from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking {
   constructor(element) {
@@ -20,18 +22,29 @@ class Booking {
 
     thisBooking.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
+    thisBooking.dom.datePicker = document.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hoursPicker = document.querySelector(select.widgets.hourPicker.wrapper);
   }
 
   initWidgets() {
     const thisBooking = this;
 
-    console.log(thisBooking.dom);
     thisBooking.peopleAmountWidget = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.dom.peopleAmount.addEventListener('updated', () => {
 
     });
 
     thisBooking.hoursAmountWidget = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.dom.hoursAmount.addEventListener('updated', () => {
+
+    });
+
+    thisBooking.datePickerWidget = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.dom.hoursAmount.addEventListener('updated', () => {
+
+    });
+
+    thisBooking.hoursPickerWidget = new HourPicker(thisBooking.dom.hoursPicker);
     thisBooking.dom.hoursAmount.addEventListener('updated', () => {
 
     });
